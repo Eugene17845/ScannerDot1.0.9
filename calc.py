de0f947f76ee507ccb53fe75327c7e5c2d3276e3
Ant <razdolbai55h@gmail.com>
2025-07-23 15:10:15 +0300

    calc.py

--- calc.py
+++ calc.py
@@ -1,6 +1,6 @@
 import tkinter as tk
 from tkinter import messagebox
-from PIL.ImageChops import difference
+
 
 
 def subtract():
@@ -15,26 +15,34 @@ def subtract():
             lambda_gz = 299792458 / (average_gz * 1000000)
             points = num3 / round(lambda_gz / 2, 3)
             points_gz = difference_gz / num4
+            lambda_num2 = int(((299792458 / (num2 * 1000000)) / 2)*100)/100
+            points_2 = num3 / float(lambda_num2)
 
             result_label.config(text=f"Расстояние(М): {round(4 * lambda_gz, 3)}")
-            result_points.config(text=f"Точки(поле): {round(points, 1)}")
+            lambda_label.config(text=f"λ: {round(lambda_gz, 3)}")
+            result_points.config(text=f"Точки(поле): {round(points, 1)}; {round(points_2,3)}")
             result_points_gz.config(text=f"Точки(частота): {round(points_gz, 1)}")
         else:
+            if num2 == 0:
+                num2 = num1
             difference_gz = num1 - num2
             average_gz = min(num1, num2) + (difference_gz / 2)
             lambda_gz = 299792458 / (average_gz * 1000000)
             points = num3 / round(lambda_gz / 2, 3)
-            points_gz = num4 / lambda_gz
+            points_gz = difference_gz / num4
+            lambda_num2 = int(((299792458 / (num1 * 1000000)) / 2) * 100) / 100
+            points_2 = num3 / float(lambda_num2)
 
             result_label.config(text=f"Расстояние(М): {round(4 * lambda_gz, 3)}")
-            result_points.config(text=f"Точки(поле): {round(points, 1)}")
+            lambda_label.config(text=f"λ: {round(lambda_gz, 3)}")
+            result_points.config(text=f"Точки(поле): {round(points, 1)}; {round(points_2, 3)}")
             result_points_gz.config(text=f"Точки(частота): {round(points_gz, 1)}")
     except ValueError:
         messagebox.showerror("Ошибка", "Пожалуйста, введите корректные числа")
 
 
 root = tk.Tk()
-root.title("СканерТЧК")
+root.title("ScannerDot")
 root.resizable(width=False, height=False)
 
 tk.Label(root, text="Первая частота(МГц):").grid(row=0, column=0, padx=10, pady=5)
@@ -44,14 +52,17 @@ entry1.grid(row=0, column=1, padx=10, pady=5)
 tk.Label(root, text="Вторая частота(МГц):").grid(row=1, column=0, padx=10, pady=5)
 entry2 = tk.Entry(root)
 entry2.grid(row=1, column=1, padx=10, pady=5)
+entry2.insert(0, '0')
 
 tk.Label(root, text="Поле(М):").grid(row=2, column=0, padx=10, pady=5)
 entry3 = tk.Entry(root)
 entry3.grid(row=2, column=1, padx=10, pady=5)
+entry3.insert(0, '0')
 
 tk.Label(root, text="Шаг(МГц):").grid(row=3, column=0, padx=10, pady=5)
 entry4 = tk.Entry(root)
 entry4.grid(row=3, column=1, padx=10, pady=5)
+entry4.insert(0, '1')
 
 subtract_button = tk.Button(root, text="Вычислить", command=subtract)
 subtract_button.grid(row=4, column=0, columnspan=2, pady=10)
@@ -59,6 +70,9 @@ subtract_button.grid(row=4, column=0, columnspan=2, pady=10)
 
 result_label = tk.Label(root, text="Расстояние(М): ")
 result_label.grid(row=5, column=0, columnspan=1)
+
+lambda_label = tk.Label(root, text="λ: ")
+lambda_label.grid(row=5, column=1, columnspan=1)
 
 result_points = tk.Label(root, text="Точки(поле): ")
 result_points.grid(row=6, column=0, columnspan=1)
